1048c11ee554530c6296d0590796077ae558704a
MiraeChoi <pnenv@naver.com>
2021-02-21 01:12:43 +0900

    Initial commit

---
+++ src/main/java/mr/mbti/controller/MainController.java
@@ -0,0 +1,13 @@
+package mr.mbti.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/test")
+    public String test1() {
+        return "hihi 한글도 잘 나오나";
+    }
+}

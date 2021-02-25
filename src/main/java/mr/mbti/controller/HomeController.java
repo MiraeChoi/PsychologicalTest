177f3522a868a4bca3e0939298344ee4a0b756cb
MiraeChoi <pnenv@naver.com>
2021-02-25 23:52:11 +0900

    edit image path

--- src/main/java/mr/mbti/controller/HomeController.java
+++ src/main/java/mr/mbti/controller/HomeController.java
@@ -1,13 +1,17 @@
 package mr.mbti.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+@Slf4j
+public class HomeController {
 
-    @RequestMapping("/main")
-    public String test1() {
+    @RequestMapping("/")
+    public String home() {
+        log.info("Home Controller!!! 와아앙ㅇ");
         return "home";
     }
 }
+

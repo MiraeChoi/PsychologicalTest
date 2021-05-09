bbbc55d375f6059a458d52073b27e7c222ac793f
MiraeChoi <pnenv@naver.com>
2021-05-09 23:56:55 +0900

    update answer logic

--- src/main/java/mr/mbti/repository/UserJpaRepository.java
+++ src/main/java/mr/mbti/repository/UserJpaRepository.java
@@ -1,11 +1,13 @@
 package mr.mbti.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
 import mr.mbti.entity.UserData;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
+@Slf4j
 public class UserJpaRepository {
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
@@ -17,5 +19,6 @@ public class UserJpaRepository {
 
     public void save(UserData userData) {
         em.persist(userData);
+        log.info("저장" + userData.getUsername());
     }
 }

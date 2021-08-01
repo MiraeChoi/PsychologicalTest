4f5673472ae4ef82ad84711b748687430313da1b
MiraeChoi <pnenv@naver.com>
2021-08-01 23:59:26 +0900

    update result logic

--- src/main/java/mr/mbti/controller/HomeController.java
+++ src/main/java/mr/mbti/controller/HomeController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mr.mbti.entity.UserData;
 import mr.mbti.repository.UserJpaRepository;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
+import javax.websocket.Session;
 import java.text.DecimalFormat;
 import java.util.*;
 
